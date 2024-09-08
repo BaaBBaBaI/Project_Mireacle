9e9196eb012f3305137571a0d43a3af9bc3efed6
BaaBBaBaI <113802612+BaaBBaBaI@users.noreply.github.com>
2024-09-08 21:25:41 +0300

    0.0.2
    hiyhihihihihj

--- Chorde method/main.cpp
+++ Chorde method/main.cpp
@@ -12,23 +12,22 @@ float f(float x) {
 
 std::pair<int, float> chordMethod(float ast, float bnd) {
     std::ofstream MyFile("info.txt");
-    int time = 0;
-    float xc;
-    while (bnd - ast > 0.0001) {
-        xc = ast - f(ast) * ((bnd - ast) / (f(bnd) - f(ast)));
-        MyFile << xc << "\n";
-        if (pow(xc, 2) > bnd) { bnd = xc; }
-        else { ast = xc; }
-        time++;
-        MyFile << time << " " << ast << " " << bnd << " " << xc << std::endl;
-    }
+    static int time = 0;
+    float xc = 10000;
+    gay:
+        xc = ast - f(ast) * (bnd - ast) / (f(bnd) - f(ast));
+            MyFile << xc << "\n";
+            ast = xc;
+            time++;
+            MyFile << time << " " << ast << " " << bnd << " " << xc << std::endl;
+    if (std::abs(xc - ast) > 0.0001) { goto gay; }
     return std::make_pair(time, xc);
 }
 
 int main() {
 
-    double ast = 0; double bnd = 20;
-    std::pair<int, double> chan;
+    float ast = 0; float bnd = 20;
+    std::pair<int, float> chan;
     
     chan = chordMethod(ast, bnd);
 
@@ -38,23 +37,13 @@ int main() {
 }
 
 
-std::pair<int, float> nonChordMethod(float ast, float bnd) {
-    std::ofstream MyFile("info_non_chord.txt");
-    int time = 0;
-    float xc = 1;
-while (fabs((bnd - ast)/xc) > 0.0001) {
-    xc = (ast + bnd)/2;
-    if (f(xc) * f(ast) > 0) {
-        ast = xc;
-        f(ast);
-    }
-
-
-
-
-
-
-}
-
-
-}
+// std::pair<int, float> nonChordMethod(float ast, float bnd) {
+//     std::ofstream MyFile("info_non_chord.txt");
+//     int time = 0;
+//     float xc = 1;
+// while (fabs((bnd - ast)/xc) > 0.0001) {
+//     xc = (ast + bnd)/2;
+//     if (f(xc) * f(ast) > 0) {
+//         ast = xc;
+//         f(ast);
+//     }
