7360a7268f600b0214496bd85c7d821c211829a3
BaaBBaBaI <113802612+BaaBBaBaI@users.noreply.github.com>
2024-09-16 01:38:46 +0300

    0.0.1.3.3
    T3d1 Not finished, finish by 0.0.1.4
    T3d1 -> kfc
    T2d3 -> kulak

--- contest1/basic.cpp
+++ contest1/basic.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 
 bool strnum(const std::string& str) {
@@ -69,7 +70,56 @@ void tatyana(int a, int b) { // IS FINISHED
 
 }
 
-int main() {
+int kulak(const int a) {
+    int b, count = 0;
+    std::vector<int> ab;
+    for (int i = 0; i < a; i++) {
+        std::cin >> b;
+        ab.push_back(b);
+    }
+    if (ab.size() < 2) { return ab.size(); }
+    if (ab[0] > ab[1]) {count++; /* std::cout << "Condition met on line: " << __LINE__ << " " << __FILE__ << ab[0] << " " << ab[1] << std::endl; */}
+    if (ab[a-1] > ab[a-2]) {count++; /* std::cout << "Condition met on line: " << __LINE__ << " " << __FILE__ << ab[a-1] << " " << ab[a-2] << std::endl; */}
+    for (int i = 1; i < a - 1; i++) {
+        if (ab[i] > ab[i-1] && ab[i] > ab[i+1]) {
+            // std::cout << "Condition met on line: " << __LINE__ << " " << __FILE__ << ab[i+1] << " " << ab[i-1] << " " << ab[i] << std::endl;
+            count++;
+        }
+    }
+    return count;
+
+}
+
+int kfc(int a) {
+    int b, count = 0;
+    std::vector<int> ab;
+    for (int i = 0; i < a; i++) {
+        std::cin >> b;
+        ab.push_back(b);
+    }
+    if (ab.size() < 2) { return 0; }
+    while (ab.size() >= 2 ) {
+    if ((ab.size() == 2) && (ab[0] == ab[1])) {return count + 2;} else if (ab.size() == 2) { return count;}
+    for (int i = 1; i < ab.size() - 1; i++) {
+        if (ab[i] == ab[i-1] ) {
+            std::cout << "Condition met on line: " << __LINE__ << " " << __FILE__ << ab.size() << " " << ab[i+1] << " " << ab[i-1] << " " << ab[i] << std::endl;
+            ab.erase(ab.begin() + i); ab.erase(ab.begin() + i - 1);
+            count = count + 2;
+
+        }
+        if (ab[i] == ab[i+1]) {
+            std::cout << "Condition met on line: " << __LINE__ << " " << __FILE__ << ab.size() << " " << ab[i+1] << " " << ab[i-1] << " " << ab[i] << std::endl;
+            ab.erase(ab.begin() + i); ab.erase(ab.begin() + i + 1);
+            count = count + 2;
+
+        }
+    }
+    }
+    return count;
+}
+
+
+void T13d3() {
     float a, b, ans = 10;
     int nd, st;
     std::string d;
@@ -80,5 +130,35 @@ int main() {
     // int c = platiti(a, b);
     // std::cout << c;
     // tatyana(a, b);
+
+}
+
+
+void T2d3() {
+    int a;
+    std::cin >> a;
+    std::cout  << kulak(a) << std::endl;
+}
+
+/* Группа студентов, состоящая из N человек решает, после какой пары они хотели бы пойти в КФС. Если студенты, желающие уйти с одной и той же пары, стоят рядом, то они уходят, а остальные продвигаются друг к другу.
+
+Например, 1 2 3 3 2 1 сначала превратится в последовательность 1 2 2 1, потом в 1 1, а потом не останется никого.
+
+Спрашивается: сколько студентов всего найдут себе компанию и дойдут до КФС?
+
+Например, для приведённой последовательности ответом будет число 6. */
+
+void T3d1() {
+    int a;
+    std::cin >> a;
+    std::cout << kfc(a) << std::endl;
+
+}
+
+int main() {
+
+    T3d1();
+
     return 0;
+    
 }
