7ced019c807f805de2c8cfe8c33c0d2c16fd42bb
BaaBBaBaI <113802612+BaaBBaBaI@users.noreply.github.com>
2024-10-21 01:02:34 +0300

    0.0.1.7
    685a9460b2255d85488ff7c151591278988f00b1

--- Method3/1.cpp
+++ Method3/1.cpp
@@ -4,80 +4,7 @@
 #include <cmath>
 #include <thread>
 #include <cassert>
-#include "color.h"
-
-int toInt(const std::vector<int>& vec) {
-    int result = 0;
-    for (const auto& digit : vec) {
-        result = result * 10 + digit;
-    }
-    return result;
-}
-
-std::vector<int> toVector(std::string a) {
-    std::vector<int> retVal;
-    for (int i = 0; i < a.length(); i++ ) {
-        retVal.push_back(a[i] - '0');
-    }
-    return retVal;
-}
-std::pair<std::vector<int>, std::vector<int>> toVecPlus(std::string aint, std::string bint, char o) {
-    /* 's' for standart vectorization of 2 strings, 'a' for advanced */
-    switch (o)
-    {
-    case 's': {
-        return std::make_pair( toVector(aint), toVector(bint));
-        break;
-    }
-    case 'a': {
-        std::vector<int> a = toVector(aint); std::vector<int> b = toVector(bint);
-        while (a.size() != b.size()) {
-            if (a.size() > b.size()) { b.insert(b.begin(), 0); }
-            if (a.size() < b.size()) { a.insert(b.begin(), 0); }
-        } return std::make_pair(a, b);
-        break;
-    }
-    default:
-        std::cout << YELLOW << "Warning: wrong char provided. Use 's' for standart vectorization of 2 strings, 'a' for advanced.\n "  << RESET;
-        break;
-    }
-    return std::pair<std::vector<int>, std::vector<int>> {-1, -1};
-}
-
-/* 
-std::vector<int> moreMoreMore (const std::string& a, const std::string& b) {
-    bool giga;
-    std::pair<std::vector<int>, std::vector<int>> woo; int over1, over2 = 0; std::vector<int> out;
-    std::reverse(woo.first.begin(), woo.first.end());
-    std::reverse(woo.second.begin(), woo.second.end());
-    std::vector<std::vector<int>> J;
-    woo = toVecPlus(a, b, 's');
-    if (woo.first.size() >= woo.second.size()) {
-        J[woo.second.size()];
-        for (int i = 0; i < woo.second.size(); i++) {
-            // for (int zz = 0; zz < i; zz++) { J[i] = 0; }
-            for (int j = 0; j < woo.first.size(); j++) {
-                over1 = woo.first[j] * woo.second[i] + over2;
-                J[i].push_back(over1 % 10); over2 = over1 / 10;
-            }
-            if (over2 != 0) { J[i].push_back(over2); }   
-        } 
-    } else {
-        J[woo.first.size()];
-        for (int i = 0; i < woo.first.size(); i++) {
-            for (int zz = 0; zz < i; zz++) { J[i].push_back(0); }
-            for (int j = 0; j < woo.second.size(); j++) {
-                over1 = woo.first[j] * woo.second[i] + over2;
-                J[i].push_back(over1 % 10); over2 = over1 / 10;
-            }
-            if (over2 != 0) { J[i].push_back(over2); }   
-        } 
-    }
-    
-    
-    
-    
-} */
+#include "method3.h"
 
 std::vector<int> moreMoreMore (const std::string& a, const std::string& b) {
     std::pair<std::vector<int>, std::vector<int>> woo;
@@ -163,7 +90,6 @@ int breather (const std::string& a, const std::string& b, char z) {
     return(toInt(outer));
 
 }
-
 
 int main() {
     std::string a, b; char z;
---
+++ Method3/method3.h
@@ -0,0 +1,59 @@
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define YELLOW "\033[33m"
+#define GREEN "\033[32m"
+
+#ifndef METHOD3_H
+#define METHOD3_H
+
+#include <iostream>
+
+std::vector<int> toVector(std::string a) {
+    std::vector<int> retVal;
+    for (int i = 0; i < a.length(); i++ ) {
+        retVal.push_back(a[i] - '0');
+    }
+    return retVal;
+}
+
+
+int toInt(const std::vector<int>& vec) {
+    int result = 0;
+    for (const auto& digit : vec) {
+        result = result * 10 + digit;
+    }
+    return result;
+}
+
+std::pair<std::vector<int>, std::vector<int>> toVecPlus(std::string aint, std::string bint, char o) {
+    /* 's' for standart vectorization of 2 strings, 'a' for advanced */
+    switch (o)
+    {
+    case 's': {
+        return std::make_pair( toVector(aint), toVector(bint));
+        break;
+    }
+    case 'a': {
+        std::vector<int> a = toVector(aint); std::vector<int> b = toVector(bint);
+        while (a.size() != b.size()) {
+            if (a.size() > b.size()) { b.insert(b.begin(), 0); }
+            if (a.size() < b.size()) { a.insert(b.begin(), 0); }
+        } return std::make_pair(a, b);
+        break;
+    }
+    default:
+        std::cout << YELLOW << "Warning: wrong char provided. Use 's' for standart vectorization of 2 strings, 'a' for advanced.\n "  << RESET;
+        break;
+    }
+    return std::pair<std::vector<int>, std::vector<int>> {-1, -1};
+}
+
+template<typename T1, typename T2>
+void swapPair(std::pair<T1, T2>& p) {
+    T1 temp = p.first;  
+    p.first = p.second;  
+    p.second = temp;  
+}
+
+#endif
